ad4b78892c8d79400dc0d0e583af34c6013cbaf6
David Araújo <david2araujo5@gmail.com>
2022-08-19 14:36:28 +0100

    Update Contacts.dart

--- ephscores/lib/Contacts.dart
+++ ephscores/lib/Contacts.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'components/Contact.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
@@ -15,9 +16,9 @@ class Contacts extends StatefulWidget {
 }
 
 class _ContactsState extends State<Contacts> {
+  List<Map<String, dynamic>> contacts = <Map<String, dynamic>>[];
+  List<Map<String, dynamic>> filtered = <Map<String, dynamic>>[];
   JsonDecoder decoder = JsonDecoder();
-  final contacts = <Contact>[];
-  final filtered = <Contact>[];
 
   // TODO Write local database if non-existing
   // TODO Create list of contacts
@@ -26,99 +27,111 @@ class _ContactsState extends State<Contacts> {
   Future<bool> _loadAsset() async {
     String value = await rootBundle.loadString('assets/Contacts.json');
     for (Map<String, dynamic> x in decoder.convert(value)["contacts"]) {
-      contacts.add(Contact(
-        title: x["title"],
-        description: x["description"],
-        number: x["number"],
-      ));
+      contacts.add(x);
+      filtered.add(x);
     }
+    
     return true;
   }
 
   @override
   void initState() {
     super.initState();
-    _loadAsset().then((bool value) => {
-          if (value)
-            {
-              _updateList("")
-            }
-        });
-  }
-
-  void _updateList(String e) {
-    setState(() {
-      filtered.clear();
-      for (Contact x in contacts) {
-        if (x.title.toLowerCase().contains(e.toLowerCase())) {
-          filtered.add(x);
-        }
-      }
-    });
+    _loadAsset().then((value) => setState(() {}));
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!contacts.isEmpty) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          foregroundColor: Color.fromRGBO(44, 73, 108, 1.0),
-          shadowColor: Colors.transparent,
-          title: Text(
-            "Contactos Importantes",
-            style: TextStyle(
-              fontSize: 24,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Color.fromRGBO(44, 73, 108, 1.0),
+        shadowColor: Colors.transparent,
+        title: Text(
+          "Contactos Importantes",
+          style: TextStyle(
+            fontSize: 24,
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 16),
-                child: TextField(
-                  style: TextStyle(color: Color.fromRGBO(44, 73, 108, 1.0)),
-                  cursorColor: Color.fromRGBO(44, 73, 108, 1.0),
-                  decoration: InputDecoration(
-                      labelText: "Procurar",
-                      labelStyle:
-                          TextStyle(color: Color.fromRGBO(44, 73, 108, 1.0)),
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Color.fromRGBO(44, 73, 108, 1.0)),
-                      ),
-                      suffixIcon: Icon(
-                        Icons.search,
-                        color: Color.fromRGBO(44, 73, 108, 1.0),
-                      )),
-                  onChanged: _updateList,
-                ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 16),
+              child: TextField(
+                style: TextStyle(color: Color.fromRGBO(44, 73, 108, 1.0)),
+                cursorColor: Color.fromRGBO(44, 73, 108, 1.0),
+                decoration: InputDecoration(
+                    labelText: "Procurar",
+                    labelStyle:
+                        TextStyle(color: Color.fromRGBO(44, 73, 108, 1.0)),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromRGBO(44, 73, 108, 1.0)),
+                    ),
+                    suffixIcon: Icon(
+                      Icons.search,
+                      color: Color.fromRGBO(44, 73, 108, 1.0),
+                    )),
+                onChanged: (e) {
+                  print(contacts[0]["title"]);
+                  setState(() {
+                    filtered = contacts.where((element) =>
+                        element["title"].toLowerCase().contains(e.toLowerCase())).toList();
+                  });
+                },
               ),
-              Flexible(
-                child: ListView(shrinkWrap: true, children: filtered),
-              ),
-            ],
-          ),
-        ),
-      );
-    } else {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          foregroundColor: Color.fromRGBO(44, 73, 108, 1.0),
-          shadowColor: Colors.transparent,
-          title: Text(
-            "Contactos Importantes",
-            style: TextStyle(
-              fontSize: 24,
             ),
-          ),
+            Flexible(
+              child: ListView.builder(
+                itemCount: filtered.length,
+                itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.grey.shade300,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListTile(
+                          title: Text(
+                            filtered[index]["title"],
+                            style: TextStyle(
+                              fontSize: 26,
+                            ),
+                          ),
+                          subtitle: Text(
+                            filtered[index]["description"],
+                          ),
+                          textColor: Color.fromRGBO(44, 73, 108, 1.0),
+                          trailing: GestureDetector(
+                            child: Icon(Icons.phone,
+                                color: Color.fromRGBO(44, 73, 108, 1.0),
+                                size: 36),
+                            onTap: () async {
+                              final Uri launchUri = Uri(
+                                scheme: 'tel',
+                                path: filtered[index]["number"],
+                              );
+                              await launchUrl(launchUri);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ],
         ),
-        body: Text("Loading"),
-      );
-    }
+      ),
+    );
   }
 }
